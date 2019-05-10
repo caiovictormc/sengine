60419519d77acb73cd07647ec456efc2c2a70e50
caiovictormc <caio@retake.com.br>
2019-05-10 13:34:45 -0300

    src.graphics.window: Get keys

--- src/graphics/window.cpp
+++ src/graphics/window.cpp
@@ -1,7 +1,15 @@
 #include "window.h"
 
 namespace sengine { namespace graphics {
-        void windowResize(GLFWwindow *window, int width, int height);
+        bool Window::m_Keys[MAX_KEYS];
+        bool Window::m_MouseButton[MAX_BUTTONS];
+        double Window::my;
+        double Window::mx;
+        void window_resize(GLFWwindow *window, int width, int height);
+        void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods);
+        void mouse_button_callback(GLFWwindow *window, int button, int action, int mods);
+        void cursor_position_callback(GLFWwindow *window, double xpos, double ypos);
+        // void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods); 
 
         Window::Window(int width, int height, const char *title) {
             m_Title = title;
@@ -9,7 +17,14 @@ namespace sengine { namespace graphics {
             m_Width = width;
             if (!init())
                 glfwTerminate();
-            glfwSetWindowSizeCallback(m_Window, windowResize);
+            glfwSetWindowSizeCallback(m_Window, window_resize);
+
+            for (int i=0; i < MAX_KEYS; i++) {
+                m_Keys[i] = false;
+            }
+            for (int i=0; i < MAX_BUTTONS; i++) {
+                m_MouseButton [i] = false;
+            }
         }
 
         Window::~Window() {
@@ -29,6 +44,10 @@ namespace sengine { namespace graphics {
                 return false;
             }
             glfwMakeContextCurrent(m_Window);
+            glfwSetWindowUserPointer(m_Window, this);
+            glfwSetKeyCallback(m_Window, key_callback);
+            glfwSetMouseButtonCallback(m_Window, mouse_button_callback);
+            glfwSetCursorPosCallback(m_Window, cursor_position_callback);
 
             if (glewInit() != GLEW_OK) {
                 std::cout << "Failed to initialze GLEW" << std::endl;
@@ -63,7 +82,40 @@ namespace sengine { namespace graphics {
             #endif
         }
 
-        void windowResize(GLFWwindow *window, int width, int height) {
+        bool Window::isKeyPressed(unsigned int keycode) {
+            if (keycode >= MAX_KEYS)
+                return false;
+            return m_Keys[keycode];
+        }
+
+        bool Window::isButtonMousePressed(unsigned int button) {
+            if (button >= MAX_BUTTONS)
+                return false;
+            return m_MouseButton[button];
+        }
+
+        void Window::getMousePosition(double &x, double &y) {
+            x = mx;
+            y = mx;
+        }
+
+        void window_resize(GLFWwindow *window, int width, int height) {
             glViewport(0, 0, width, height);
+        }
+
+        void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods) {
+            Window *win = (Window*) glfwGetWindowUserPointer(window);
+            win->m_Keys[key] = (action != GLFW_RELEASE);
+        }
+
+        void mouse_button_callback(GLFWwindow *window, int button, int action, int mods) {
+            Window *win = (Window*) glfwGetWindowUserPointer(window);
+            win->m_MouseButton[button] = (action != GLFW_RELEASE);
+        }
+
+        void cursor_position_callback(GLFWwindow *window, double xpos, double ypos) {
+            Window *win = (Window*) glfwGetWindowUserPointer(window);
+            win->mx = xpos;
+            win->my = ypos; 
         }
 }}
--- src/graphics/window.h
+++ src/graphics/window.h
@@ -4,6 +4,10 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+#define MAX_KEYS 1024
+#define MAX_BUTTONS 32
+
+
 namespace sengine { namespace graphics {
         class Window 
         {
@@ -12,6 +16,9 @@ namespace sengine { namespace graphics {
             int m_Width, m_Height;
             GLFWwindow *m_Window;
             bool m_Closed;
+            static bool m_Keys[MAX_KEYS];
+            static bool m_MouseButton[MAX_BUTTONS];
+            static double mx, my;
         public:
             Window(int width, int height, const char *title);
             ~Window();
@@ -21,7 +28,15 @@ namespace sengine { namespace graphics {
 
             inline int getWidth() const { return m_Width; }
             inline int getHeight() const { return m_Height; }
+
+            static bool isKeyPressed(unsigned int keycode);
+            static bool isButtonMousePressed(unsigned int button);
+            static void getMousePosition(double &x, double &y);
         private:
             bool init();
+            friend void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods);
+            friend void mouse_button_callback(GLFWwindow *window, int button, int action, int mods);
+            friend void cursor_position_callback(GLFWwindow *window, double xpos, double ypos);
+            // static void callback(GLFWwindow *window, int key, int scancode, int action, int mods); 
         };
 }}
